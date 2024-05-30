dc8c3c92f31f57954258ed809b3c4decf771faac
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-05-30 17:01:31 +0900

    Update t.py

--- 1.selenium批量下载图片含有进度展示/t.py
+++ 1.selenium批量下载图片含有进度展示/t.py
@@ -21,27 +21,34 @@ from selenium.webdriver.common.by import By
 from tqdm import tqdm
 import time
 
-# 使用tqdm展示进度条
-for i in tqdm(range(1, 1200)):
-    # 模拟一些处理时间
-    time.sleep(0.01)
+# # 使用tqdm展示进度条
+# for i in tqdm(range(1, 1200)):
+#     # 模拟一些处理时间
+#     time.sleep(0.01)
+import uuid
+def generate_unique_id():
+    unique_id = str(uuid.uuid4())[:8].lower()
+    return unique_id
 
 
 driver = webdriver.Chrome()
 # 把find_elements 改为　find_element
-def get_first_page():
+def get_first_page(imgname):
 
     url = 'https://www.sg-siken.com/sgkakomon.php'
     driver.get(url)
+    # off
+    driver.find_element(By.XPATH,'//*[@id="tab1"]/div/button[2]').click()
+    driver.find_element(By.XPATH,'//*[@id="tab1"]/label[3]/span').click()
     time.sleep(3)
     driver.find_element(By.XPATH,'//*[@id="configform"]/div[2]/button').click()
     time.sleep(3)
     # 截取整个页面的屏幕截图
-    driver.save_screenshot("sg-img-first.png")
+    driver.save_screenshot("{0}-first.png".format(imgname))
     time.sleep(3)
     driver.find_element(By.XPATH, '//*[@id="showAnswerBtn"]').click()
     time.sleep(1)
-    driver.save_screenshot("sg-img-second.png")
+    driver.save_screenshot("{0}-second.png".format(imgname))
 
 
 def print_progress_percentage(current, total):
@@ -60,17 +67,17 @@ def print_progress_percentage(current, total):
 
 # 把首页和翻页处理？
 
-def next_page():
-    for i in range(1,1200):  # selenium 循环翻页成功！
-        print_progress_percentage(i, 1200)
+def next_page(imgname):
+    for i in range(1,52):  # selenium 循环翻页成功！
+        print_progress_percentage(i, 52)
         driver.find_element(By.XPATH,'//*[@id="configform"]/div[1]/button[1]').click()
         time.sleep(1)
         # 截取整个页面的屏幕截图
-        driver.save_screenshot("sg-img-{0}-1.png".format(str(i)))
+        driver.save_screenshot("{0}-{1}-1.png".format(imgname,str(i)))
         time.sleep(3)
         driver.find_element(By.XPATH, '//*[@id="showAnswerBtn"]').click()
         time.sleep(1)
-        driver.save_screenshot("sg-img-{0}-2.png".format(str(i)))
+        driver.save_screenshot("{0}-{1}-2.png".format(imgname,str(i)))
         time.sleep(3)
 
 
@@ -87,11 +94,13 @@ def next_page():
 
 
 if __name__ == '__main__':
-        html = get_first_page()
-        time.sleep(1)
+    imgname = generate_unique_id()
 
-        while True:
-            next_page()
+    html = get_first_page(imgname)
+    time.sleep(1)
+
+    while True:
+        next_page(imgname)
 
 
 
